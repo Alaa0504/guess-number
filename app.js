c9c9d0f6e176bbd30423d18fe0175afec545e8fe
Alaa <bachiest@gmail.com>
2021-06-26 14:06:52 +0800

    加入 sweetalert 的彈出訊息樣式

--- app.js
+++ app.js
@@ -6,16 +6,32 @@ btn.addEventListener('click', function (e) {
 
     const userVal = parseInt(numberInput.value, 10);
     if (userVal == randomNumber) {
-        alert('You are right!!!');
+        Swal.fire({
+            title: 'Awesome!',
+            text: 'you are Right !!!',
+            icon: 'success',
+            confirmButtonText: 'Cool'
+        })
         return;
     }
     console.log(userVal, randomNumber);
     if (userVal > randomNumber) {
-        alert('smaller');
+        Swal.fire({
+            title: 'Error!',
+            text: 'smaller!!!',
+            icon: 'error',
+            confirmButtonText: 'Cool'
+        })
         return;
     }
     if (userVal < randomNumber) {
-        alert('bigger');
+        Swal.fire({
+            title: 'Error!',
+            text: 'bigger!!!',
+            icon: 'error',
+            confirmButtonText: 'Cool'
+        })
         return;
     }
 });
+
